17d4551d787852aff234cda26cc13304b47000ec
Luis Raul Espinoza Barboza <respinozabarboza@gmail.com>
2020-05-29 18:38:19 -0600

    update rest api service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiRestService } from 'src/app/shared/services/api-rest.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   pipeValue = 'demo-pipe-woekds';
-  constructor() { }
+  data;
+  errorMessage;
+
+  constructor(private restApi: ApiRestService) { }
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  getData() {
+    this.restApi.get(environment.apis.main.host, '',
+    'public-api/users?_format=json&access-token=2mjCKW5c2jtNfCdLY9T0PPv_mmu_qUeB21pW').subscribe({
+        next: res => {  // success
+          this.data = res.result;
+        },
+        error: err => { // error
+          this.errorMessage = err;
+        }
+      });
+      //   res => {
+      //   console.log(res);
+      //   this.data = res.result;
+      // }, err => {
+      //   console.log(err);
+      // });
+  }
 }
--- src/app/shared/services/api-rest.service.ts
+++ src/app/shared/services/api-rest.service.ts
@@ -1,18 +1,60 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiRestService {
 
-  apiHost = 'https://putsreq.com/8EaANchgi4aU0V704UOV'
+  apiHost = 'https://putsreq.com/8EaANchgi4aU0V704UOV';
 
   constructor(private http: HttpClient) { }
+  /**
+   * Build full host more endpoint call
+   * @param host api host server
+   * @param endPoint api endpoint
+   * @param version api version endpoint
+   */
+  private buildPath(host: string, endPoint: string, version: string = ''): string {
+    let path;
+    if (version === '') {
+      path = `${host}/${endPoint}`;
+    } else {
+      path = `${host}/${version}/${endPoint}`;
+    }
+    console.log(path);
+    return path;
+  }
+  /**
+   * GET call
+   * @param host api host server
+   * @param version api version
+   * @param endPoint endpoint name
+   */
+  get<T>(host: string, version: string, endPoint: string): Observable<any> {
+    return this.http.get<T>(this.buildPath(host, endPoint, version)).pipe(
+      tap(res => console.log('Get: ', JSON.stringify(res))),
+      catchError(this.handleError)
+    );
+    // return of(data);
+  }
 
   post(data): Observable<any> {
     return this.http.post(this.apiHost, data);
     // return of(data);
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error ocurred ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.log();
+    return throwError(errorMessage);
   }
 }
