0b70eb5cec43d81409bc980bf184bce88e896573
Luis Raul Espinoza Barboza <respinozabarboza@gmail.com>
2020-06-01 11:28:00 -0600

    add new authentication module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import { RateComponent } from './shared/components/rate/rate.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ServiceComponent } from './components/service/service.component';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
 
 @NgModule({ // decorator
-  declarations: [ // childs componnents
+  declarations: [ // childs componnents, pipes
     AppComponent,
     ContactusComponent,
     ServicesComponent,
@@ -28,15 +29,15 @@ import { ServiceComponent } from './components/service/service.component';
     NotFoundComponent,
     ServiceComponent // declare pipe
   ],
-  imports: [ // dependencies
+  imports: [ // dependencies, modules, 3th party software
     BrowserModule, // imports directives (ngFor, ngIf, etc)
     AppRoutingModule, // routing calls configuration
     AdminModule, // aditional module
     FormsModule, // imports diretcives (ngModule)
     HttpClientModule, // rest calls
-    FontAwesomeModule // imporst
+    FontAwesomeModule, AuthenticationModule // imporst
   ],
-  providers: [],
+  providers: [], // no recommend, instead use declaration Injectable -> providedIn
   bootstrap: [AppComponent] // main component to load
 })
 export class AppModule { } // name of component
